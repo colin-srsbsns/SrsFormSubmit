90e4256e7eb087e70c34036854af1badceaaf294
colin-srsbsns <colin@srsbsns.co.za>
2025-07-07 12:05:40 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,15 @@ use App\Kernel;
 $projectDir = realpath(__DIR__.'/..');   // ↔ /Users/Colin/project on your Mac
 // ↔ /usr/home/xxxxx on Xneelo
 echo $projectDir;
-require_once $projectDir.'/vendor/autoload_runtime.php';
+$isXneelo = str_starts_with($projectDir, '/usr/home/');
+
+if ($isXneelo) {
+    require_once '/usr/home/formsdpmfc/vendor/autoload_runtime.php';// server-specific logging, ini_set, etc.
+}
+else{
+    require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+}
+
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
